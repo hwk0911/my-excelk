9653835376827742f47bc88839a0d6f41dac3083
master <hwk0911@gmail.com>
2019-07-25 02:02:40 +0900

    회원가입 , 사용자 목록 구현

---
+++ STS_WorkSpace3/net_EXCELK/src/test/java/net_EXCELK/NetExcelkApplicationTests.java
@@ -0,0 +1,16 @@
+package net_EXCELK;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetExcelkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
