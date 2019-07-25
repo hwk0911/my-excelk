2d7339794f0b9df15243037b64d58b5007955187
master <hwk0911@gmail.com>
2019-07-26 08:20:50 +0900

    수정부분 PostMapping -> PutMapping 로 수정

--- STS_WorkSpace3/net_EXCELK/src/main/java/net_EXCELK/UserSignUpController.java
+++ STS_WorkSpace3/net_EXCELK/src/main/java/net_EXCELK/UserSignUpController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -41,7 +42,7 @@ public class UserSignUpController {
 		return "/user/updateForm";
 	}
 	
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public String update(@PathVariable Long id, User newUser){
 		User user = userRepository.findById(id).get();
 		user.update(newUser);
