ac5709f6d423e9cef0a9687270f6440620257404
master <hwk0911@gmail.com>
2019-07-26 07:50:15 +0900

    개인정보수정 Form, header.html include

--- STS_WorkSpace3/net_EXCELK/src/main/java/net_EXCELK/UserSignUpController.java
+++ STS_WorkSpace3/net_EXCELK/src/main/java/net_EXCELK/UserSignUpController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,5 +31,13 @@ public class UserSignUpController {
 	public String list(Model model){
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model){
+		User user = userRepository.findById(id).get();
+		System.out.println(user);
+		model.addAttribute("users", user);
+		return "/user/updateForm";
 	}
 }
