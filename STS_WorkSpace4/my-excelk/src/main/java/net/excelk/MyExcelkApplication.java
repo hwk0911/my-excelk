4d7c0aac44b725aa8dabc7c73452470878ff1be2
excelK <hwk0911@gmail.com>
2019-07-29 05:08:29 +0900

    STS_WorkSpace4 인덱스, 회원가입 페이지 구현

---
+++ STS_WorkSpace4/my-excelk/src/main/java/net/excelk/MyExcelkApplication.java
@@ -0,0 +1,13 @@
+package net.excelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExcelkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExcelkApplication.class, args);
+	}
+
+}
